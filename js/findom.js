a56f8dae5236f225fb2135f468d91905a094afe2
Deca Service <servicedeca@gmail.com>
2015-03-02 12:11:51 +0700

    process

--- js/findom.js
+++ js/findom.js
@@ -334,49 +334,45 @@ function init () {
             zoom: 12
         }),
 
-        // Создаем геообъект с типом геометрии "Точка".
-        myGeoObject = new ymaps.GeoObject({
-            // Описание геометрии.
-            geometry: {
-                type: "Point",
-                coordinates: [55.8, 37.8]
-            },
+		
+		 myPlacemark4 = new ymaps.Placemark([55.77, 37.65], {
             // Свойства.
-            properties: {
-                // Контент метки.
-        
-                balloonContent: 'Меня можно перемещать'
-            }
-        }, {
-            // Опции.
-            // Иконка метки будет растягиваться под размер ее содержимого.
-            preset: 'twirl#redStretchyIcon',
-            // Метку можно перемещать.
-            draggable: true
-        }),
-
-        // Создаем метку с помощью вспомогательного класса.
-        myPlacemark1 = new ymaps.Placemark([55.8, 37.6], {
-            // Свойства.
-            // Содержимое иконки, балуна и хинта.
-            iconContent: '1',
-            balloonContent: 'Балун',
-            hintContent: 'Стандартный значок метки'
-        }, {
-            // Опции.
-            // Стандартная фиолетовая иконка.
-            preset: 'twirl#violetIcon'
-        }),
-
-        myPlacemark2 = new ymaps.Placemark([55.76, 37.56], {
-            // Свойства.
-            hintContent: 'Собственный значок метки'
+            hintContent: ''
         }, {
             // Опции.
             // Своё изображение иконки метки.
-            iconImageHref: '/images/minihome.png',
+            iconImageHref: '/images/tag.png',
             // Размеры метки.
-            iconImageSize: [30, 42],
+            iconImageSize: [45, 70],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-3, -42]
+        });
+	
+		myPlacemark1 = new ymaps.Placemark([55.8, 37.6], {
+            // Свойства.
+            hintContent: ''
+        }, {
+            // Опции.
+            // Своё изображение иконки метки.
+            iconImageHref: '/images/tag.png',
+            // Размеры метки.
+            iconImageSize: [45, 70],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-3, -42]
+        });
+	
+
+        myPlacemark2 = new ymaps.Placemark([55.76, 37.56], {
+            // Свойства.
+            hintContent: ''
+        }, {
+            // Опции.
+            // Своё изображение иконки метки.
+            iconImageHref: '/images/tag.png',
+            // Размеры метки.
+            iconImageSize: [45, 70],
             // Смещение левого верхнего угла иконки относительно
             // её "ножки" (точки привязки).
             iconImageOffset: [-3, -42]
@@ -384,9 +380,9 @@ function init () {
 
     // Добавляем все метки на карту.
     myMap.geoObjects
-        .add(myPlacemark1)
+        .add(myPlacemark4)
         .add(myPlacemark2)
-        .add(myGeoObject);
+        .add(myPlacemark1);
 }
 
 
