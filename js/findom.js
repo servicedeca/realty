421ac947bd731e4f49965fcbf254e622ebf27bbd
Deca Service <servicedeca@gmail.com>
2015-03-11 03:28:26 +0700

    process

--- js/findom.js
+++ js/findom.js
@@ -578,5 +578,33 @@ function init () {
     });
 
 
+		// действия после полной загрузки страницы
+		$(document).ready(function(){
+			// проверяем, какие чекбоксы активны и меняем сласс для родительского дива
+			$('.dec_checkbox').each(function(){
+				var checkbox = $(this).find('input[type=checkbox]');
+				if(checkbox.prop("checked")) $(this).addClass("check_active");
+			});
+		
+			// при клике по диву, делаем проверку
+			$('.dec_checkbox').click(function(){
+				var checkbox = $(this).find('input[type=checkbox]');
+				// если чекбокс был активен
+				if(checkbox.prop("checked")){
+					// снимаем класс с родительского дива
+					$(this).removeClass("check_active");
+					// и снимаем галочку с чекбокса
+					checkbox.prop("checked", false);
+				// если чекбокс не был активен
+				}else{
+					// добавляем класс родительскому диву
+					$(this).addClass("check_active");
+					// ставим галочку в чекбоксе
+					checkbox.prop("checked", true);
+				}
+			});
+		});
+
+
 
 
